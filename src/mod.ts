026884d9edefeeb97344da1f7c06e9d555d39c3a
mBartoldus <mbartoldus@gmail.com>
2024-06-26 00:40:44 -0400

    export type TreeReducer

--- src/mod.ts
+++ src/mod.ts
@@ -6,7 +6,7 @@ export { isPojo } from './PojoTree/isPojo.ts'
 export { climbTree, climbTrees } from './climbTree/climbTree.ts'
 export { mapTree } from './mapTree/mapTree.ts'
 export { filterTree } from './filterTree/filterTree.ts'
-export { reduceTree } from './reduceTree/reduceTree.ts'
+export { reduceTree, type TreeReducer } from './reduceTree/reduceTree.ts'
 
 export { doTree } from './doTree/doTree.ts'
 export { mergeTrees, overlayTrees, treeProduct } from './mergeTrees/mergeTrees.ts'
